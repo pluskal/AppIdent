db23f1cedaca5c2c8a6eb0ed012a98c786866bb3
Jan Pluskal <ipluskal@fit.vutbr.cz>
2017-06-07 15:21:23 +0200

    Add results 2017/06/07

---
+++ Results/Bayes1.ps1
@@ -0,0 +1,20 @@
+﻿.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.1 -s 0.2  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.1 -s 0.6  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.1 -s 0.6
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.1 -s 0.6   -n
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.1 -s 0.6   -n
+
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.2 -s 0.2  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.2 -s 0.6  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.2 -s 0.6
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.2 -s 0.6   -n
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.2 -s 0.6   -n
+
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.5 -s 0.2  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.5 -s 0.6  
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.5 -s 0.6
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\portAgregated_appIdentDataSource.bin  -b -m 30 -r 0.5 -s 0.6   -n
+.\AppIdentCli.exe -d C:\Users\pluskal\Netfox\AppIdent\all_ports_appIdentDataSource.bin      -b -m 30 -r 0.5 -s 0.6   -n
+
+
+
